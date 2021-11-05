b84eacbdee1fa72a05e76c6ab79ebbae9c9702a3
Ferreiramg <lpdeveloper@hotmail.com,br>
2021-11-04 23:02:55 -0300

    Update e Deletar Logico

--- Models/Colaborador.cs
+++ Models/Colaborador.cs
@@ -6,6 +6,7 @@ namespace projetoTeste.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public string? Deleteat { get; set; }
         public float Salario { get; set; }
 
         public int IdCargo { get; set; }
